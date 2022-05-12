00a82ca7f35d367b008a155d17295d920d9ef136
Sam Wilson <iduckyfuzz@gmail.com>
2022-05-12 15:41:02 +0100

    maintaining quiz working

--- client/src/Components/Edit/Edit.js
+++ client/src/Components/Edit/Edit.js
@@ -8,6 +8,10 @@ const Body = styled.div`
   text-align: center;
 `;
 
+const StyledInput = styled.input`
+  width: 40%;
+`;
+
 const Edit = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
@@ -36,26 +40,30 @@ const Edit = () => {
     setQuizUpdated(true);
   };
 
-  const deleteQuestion = (e) => {
-    console.log("deleteQuestion ", quiz.questions[e]);
-    console.log(e);
+  const addQuestion = (i) => {
+    let array = quiz;
+    if (
+      array.questions[i].incorrect_answers.length +
+        array.questions[i].correct_answers.length !==
+      5
+    ) {
+      array.questions[i].incorrect_answers.push("");
+      setQuiz({ ...array });
+    } else {
+      console.log("maximum of answers reached");
+    }
+  };
 
+  const deleteQuestion = (e) => {
     let array = quiz;
     console.log(e);
-    if (array.questions.includes(quiz.questions[e])) {
-      array.questions.splice(e, 1);
-    }
-    setQuiz(array);
+    array.questions.splice(e, 1);
+    setQuiz({ ...array });
     setQuizUpdated(false);
   };
 
   const deleteAnswer = (i, answer) => {
-    console.log("deleteAnswer", i, answer);
-
     let array = quiz;
-
-    console.log(i, answer);
-
     if (array.questions[i].correct_answers.includes(answer)) {
       let index = array.questions[i].correct_answers.indexOf(answer);
       array.questions[i].correct_answers.splice(index, 1);
@@ -64,7 +72,7 @@ const Edit = () => {
       array.questions[i].incorrect_answers.splice(index, 1);
     }
 
-    setQuiz(array);
+    setQuiz({ ...array });
     setQuizUpdated(false);
   };
 
@@ -78,45 +86,50 @@ const Edit = () => {
       <Body>
         <h1>Edit</h1>
         <div>
-          <input defaultValue={quiz.title}></input>
+          <StyledInput defaultValue={quiz.title}></StyledInput>
         </div>
         {quiz.questions.map((questions, index) => {
           return (
-            <div>
-              <input
-                defaultValue={questions.question}
-                onChange={(e) => {
-                  console.log(e.target.value);
-                }}
-              ></input>
-              <button onClick={() => deleteQuestion(index)}>x</button>
-              {questions.correct_answers
-                .concat(questions.incorrect_answers)
-                .map((answer) => {
-                  return (
-                    <div>
-                      <input
-                        defaultValue={answer}
-                        onChange={(e) => {
-                          console.log(e.target.value);
-                        }}
-                      ></input>
-                      <input
-                        defaultChecked={questions.correct_answers.includes(
-                          answer
-                        )}
-                        onChange={(e) => {
-                          console.log(e.target.checked);
-                        }}
-                        type="checkbox"
-                      ></input>
-                      <button onClick={() => deleteAnswer(index, answer)}>
-                        x
-                      </button>
-                    </div>
-                  );
-                })}
-            </div>
+            <>
+              <div>
+                <StyledInput
+                  defaultValue={questions.question}
+                  key={questions.question}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                  }}
+                ></StyledInput>
+                <button onClick={() => deleteQuestion(index)}>x</button>
+                {questions.correct_answers
+                  .concat(questions.incorrect_answers)
+                  .map((answer) => {
+                    return (
+                      <div>
+                        <input
+                          defaultValue={answer}
+                          key={answer}
+                          onChange={(e) => {
+                            console.log(e.target.value);
+                          }}
+                        ></input>
+                        <input
+                          defaultChecked={questions.correct_answers.includes(
+                            answer
+                          )}
+                          onChange={(e) => {
+                            console.log(e.target.checked);
+                          }}
+                          type="checkbox"
+                        ></input>
+                        <button onClick={() => deleteAnswer(index, answer)}>
+                          x
+                        </button>
+                      </div>
+                    );
+                  })}
+                <button onClick={() => addQuestion(index)}>+</button>
+              </div>
+            </>
           );
         })}
         <div>
